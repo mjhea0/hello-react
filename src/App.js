10136c5253fc91a4fce4a1dc9cb68f75c505ca6e
Michael Herman <hermanmu@gmail.com>
2016-11-10 10:41:54 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Name from './Name';
+
+// const name = 'Michael'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {name: 'Bob Barker'}
+  }
+
+  changeName(event) {
+    this.setState({name: event.target.value})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <input type="text" value={this.state.name } onChange={this.changeName.bind(this)}></input>
+        <Name name={this.state.name}/>
+      </div>
+    );
+  }
+}
+
+export default App;
